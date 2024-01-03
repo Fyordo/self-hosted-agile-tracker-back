cdf2ff7540264eed182e2a7e25ce99d751da5e0a
IDante <maxim.polyanskiy@mail.ru>
2024-01-04 00:14:36 +0300

    Add auth service

---
+++ src/main/java/com/rentinhand/rihtracker/services/auth/AuthService.java
@@ -0,0 +1,86 @@
+package com.rentinhand.rihtracker.services.auth;
+
+import com.rentinhand.rihtracker.dto.requests.user.AuthEmailRequest;
+import com.rentinhand.rihtracker.dto.requests.user.UserCreateRequest;
+import com.rentinhand.rihtracker.dto.responses.user.AuthResponse;
+import com.rentinhand.rihtracker.entities.Role;
+import com.rentinhand.rihtracker.entities.User;
+import com.rentinhand.rihtracker.repos.TokenRepository;
+import com.rentinhand.rihtracker.repos.UserRepository;
+import com.rentinhand.rihtracker.utilities.Token.Token;
+import com.rentinhand.rihtracker.utilities.Token.TokenType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository repository;
+    private final TokenRepository tokenRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthResponse register(UserCreateRequest request) {
+        var user = User.builder()
+                .username(request.getUsername())
+                .login(request.getLogin())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        var savedUser = repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        saveUserToken(savedUser, jwtToken);
+        return AuthResponse.builder()
+                .token(jwtToken)
+                .id(savedUser.getId())
+                .build();
+    }
+
+    public AuthResponse authenticateEmail(AuthEmailRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getLogin(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByLogin(request.getLogin())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        revokeAllUserTokens(user);
+        saveUserToken(user, jwtToken);
+        return AuthResponse.builder()
+                .token(jwtToken)
+                .id(user.getId())
+                .build();
+    }
+
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
+                .build();
+        tokenRepository.save(token);
+    }
+
+    private void revokeAllUserTokens(User user) {
+        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+        if (validUserTokens.isEmpty())
+            return;
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
+    }
+
+
+}
