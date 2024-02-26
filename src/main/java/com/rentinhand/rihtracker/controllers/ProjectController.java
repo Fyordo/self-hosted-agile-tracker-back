619ab6d9cb193f2786931e37bd76e86ca727a8f0
Fedor <lagutinfedya@gmail.com>
2024-02-26 19:24:19 +0300

    feat: add SecurityWorkspace to BaseController

--- src/main/java/com/rentinhand/rihtracker/controllers/ProjectController.java
+++ src/main/java/com/rentinhand/rihtracker/controllers/ProjectController.java
@@ -1,29 +1,19 @@
 package com.rentinhand.rihtracker.controllers;
 
 import com.rentinhand.rihtracker.dto.requests.project.ProjectDataRequest;
-import com.rentinhand.rihtracker.dto.requests.user.UserUpdateRequest;
 import com.rentinhand.rihtracker.dto.responses.ListResponse;
 import com.rentinhand.rihtracker.dto.responses.project.ProjectDetailResponse;
 import com.rentinhand.rihtracker.dto.responses.project.ProjectResponse;
-import com.rentinhand.rihtracker.dto.responses.user.UserResponse;
 import com.rentinhand.rihtracker.entities.Project;
-import com.rentinhand.rihtracker.entities.User;
 import com.rentinhand.rihtracker.exceptions.ModelNotFoundException;
 import com.rentinhand.rihtracker.services.ProjectService;
-import com.rentinhand.rihtracker.services.UserService;
-import com.rentinhand.rihtracker.services.auth.SecurityWorkspace;
-import com.rentinhand.rihtracker.utilities.AuthorityAnnotations.AdminAuth;
 import com.rentinhand.rihtracker.utilities.AuthorityAnnotations.Authorized;
 import com.rentinhand.rihtracker.utilities.AuthorityAnnotations.DirectorAuth;
-import com.rentinhand.rihtracker.utilities.AuthorityAnnotations.UserAuth;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/java/com/rentinhand/rihtracker/services/auth/SecurityWorkspace.java
+++ src/main/java/com/rentinhand/rihtracker/services/auth/SecurityWorkspace.java
@@ -2,6 +2,7 @@ package com.rentinhand.rihtracker.services.auth;
 
 import com.rentinhand.rihtracker.entities.User;
 import com.rentinhand.rihtracker.services.UserService;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,7 @@ public class SecurityWorkspace {
     }
 
     public Long getAuthUserId(){
-        User unlinkedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return unlinkedUser.getId();
+        Claims claims = (Claims) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return Long.getLong(claims.get("sub", String.class));
     }
 }
