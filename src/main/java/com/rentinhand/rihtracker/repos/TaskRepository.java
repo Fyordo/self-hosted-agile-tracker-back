24cbb432adf6c88d99e6892b82701c039a4c023a
Fedor <lagutinfedya@gmail.com>
2024-02-29 01:58:29 +0300

    feat: add querydsl dependency

--- src/main/java/com/rentinhand/rihtracker/repos/TaskRepository.java
+++ src/main/java/com/rentinhand/rihtracker/repos/TaskRepository.java
@@ -1,5 +1,10 @@
 package com.rentinhand.rihtracker.repos;
 
+import com.querydsl.core.types.QTuple;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.rentinhand.rihtracker.entities.ScrumColumn;
 import com.rentinhand.rihtracker.entities.Task;
 import com.rentinhand.rihtracker.entities.TaskType;
@@ -7,14 +12,16 @@ import com.rentinhand.rihtracker.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Timestamp;
+import java.util.Map;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long>, QuerydslPredicateExecutor<Task> {
     @Transactional
     @Modifying
     @Query("""
@@ -29,5 +36,4 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
             User maintainer,
             Long id
     );
-
 }
