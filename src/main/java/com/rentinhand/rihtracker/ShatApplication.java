d717d15ee68e5d889bda75005291d48bf4721f16
Fedor <lagutinfedya@gmail.com>
2024-06-15 20:33:23 +0300

    feat: change app name

--- src/main/java/com/rentinhand/rihtracker/ShatApplication.java
+++ src/main/java/com/rentinhand/rihtracker/ShatApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RihTrackerApplication {
+public class ShatApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RihTrackerApplication.class, args);
+        SpringApplication.run(ShatApplication.class, args);
     }
 
 }
